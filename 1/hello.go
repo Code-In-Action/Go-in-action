b570fa9f1113b674109f8cfcc7c58a1033edf977
dengyongyuan <dengyongyuan@yiwei.com>
2017-04-20 00:28:45 +0800

    init

--- 1/hello.go
+++ 1/hello.go
@@ -198,23 +198,63 @@
 //func main() {
 //	fmt.Println(m)
 //}
+//
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	m := make(map[string]int)
+//
+//	m["Answer"] = 42
+//	fmt.Println("The value:", m["Answer"])
+//
+//	m["Answer"] = 48
+//	fmt.Println("The value:", m["Answer"])
+//
+//	delete(m, "Answer")
+//	fmt.Println("The value:", m["Answer"])
+//
+//	v, ok := m["Answer"]
+//	fmt.Println("The value:", v, "Present?", ok)
+//}
 
+//函数
+//package main
+//
+//import (
+//	"fmt"
+//	"math"
+//)
+//
+//func main() {
+//	hypot := func(x, y float64) float64 {
+//		return math.Sqrt(x*x + y*y)
+//	}
+//
+//	fmt.Println(hypot(3, 4))
+//}
+
+//函数的闭包
+//Go 函数可以是闭包的。闭包是一个函数值，它来自函数体的外部的变量引用。 函数可以对这个引用值进行访问和赋值；换句话说这个函数被“绑定”在这个变量上。
 package main
 
 import "fmt"
 
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
 func main() {
-	m := make(map[string]int)
-
-	m["Answer"] = 42
-	fmt.Println("The value:", m["Answer"])
-
-	m["Answer"] = 48
-	fmt.Println("The value:", m["Answer"])
-
-	delete(m, "Answer")
-	fmt.Println("The value:", m["Answer"])
-
-	v, ok := m["Answer"]
-	fmt.Println("The value:", v, "Present?", ok)
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
 }
